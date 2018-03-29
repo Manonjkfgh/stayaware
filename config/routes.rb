f6dab310590f040cd6fed615aaaea38db2b9fe2e
Manon Riou <manon@MacBook-Pro-de-Manon.local>
2018-03-29 16:13:04 +0200

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :series
   resources :films
   resources :sites
 root 'sites#index'
 root 'films#index'
+root 'series#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
